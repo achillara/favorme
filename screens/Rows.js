6d54540f18a723550e6aaa15dfdcfde472d59a20
Anu <srianu98@yahoo.com>
2017-09-09 13:50:02 -0400

    more row fixing

--- screens/Rows.js
+++ screens/Rows.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { View, Text, StyleSheet, Image } from 'react-native';
 
+
 const styles = StyleSheet.create({
   container: {
     flex: 1,
@@ -19,12 +20,47 @@ const styles = StyleSheet.create({
   },
 });
 
+const rightstyles = StyleSheet.create({
+  container: {
+    flex: 1,
+    padding: 12,
+    flexDirection: 'row',
+  }
+});
+const newright = StyleSheet.create({
+  container:{
+    flex: 1,
+    marginRight: 3,
+    flexDirection: 'row',
+    alignItems:'flex-end',
+  }
+});
+
 const Row = (props) => (
+  <View style = {{flexDirection:"row"}}>
   <View style={styles.container}>
     <Image source={{ uri: props.picture.large}} style={styles.photo} />
     <Text style={styles.text}>
-      {`${props.name.first} ${props.name.last}`}
+      {`${props.name.first}`}
     </Text>
+  </View>
+  <View style = {styles.container}>
+  <Text style={styles.text}>
+      {`${props.score}`}
+  </Text>
+  </View>
+
+  <View style={rightstyles.container}>
+  <Image source={{ uri: "https://randomuser.me/api/portraits/women/22.jpg"}} style={styles.photo} />
+    <Text style={styles.text}>
+      Anusha 
+    </Text>
+  </View>
+  <View style = {newright.container}>
+    <Text style={styles.text}>
+     0
+    </Text>
+  </View>
   </View>
 );
 
