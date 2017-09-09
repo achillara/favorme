64ae800a862a2958bb30beec5943b231c2b8c871
Anu <srianu98@yahoo.com>
2017-09-09 01:39:59 -0400

    added Facebook login

--- App.js
+++ App.js
@@ -1,11 +1,41 @@
+import 'expo';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+
+import { StyleSheet, Text, View, Button } from 'react-native';
 
 export default class App extends React.Component {
+  
+  authenticate = (token) => {
+    const provider = firebase.auth.FacebookAuthProvider;
+    const credential = provider.credential(token);
+    return firebase.auth().signInWithCredential(credential);
+  }
+
+  login = async () => {
+    const ADD_ID = '130629304233835';
+    const options = {
+        permissions: ['public_profile', 'email'],
+    }
+    const {type, token} = await Expo.Facebook.logInWithReadPermissionsAsync(ADD_ID, options)
+    if (type === 'success') {
+         const response = await fetch(`https://graph.facebook.com/me?access_token=${token}`)
+         console.log(await response.json());
+        this.authenticate(token);
+    }
+}
+
+
+
+
   render() {
     return (
       <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
+      <Button
+          raised
+          onPress={this.login}
+          icon={{name: 'cached'}}
+          title='Login With Facebook' />
+        <Text>Welcome to Favor.Me!</Text>
       </View>
     );
   }
