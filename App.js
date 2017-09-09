cce7947007e95db88ae544ce1795dc8f7d9ef7fb
nelsondude <alexn1336@gmail.com>
2017-09-09 17:03:08 -0400

    Working API Request

--- App.js
+++ App.js
@@ -1,7 +1,7 @@
 import 'expo';
 import React from 'react';
 
-import { StyleSheet, Text, View, Button } from 'react-native';
+import { StyleSheet, Text, View, Button, AsyncStorage } from 'react-native';
 
 export default class App extends React.Component {
   
@@ -19,11 +19,30 @@ export default class App extends React.Component {
     const {type, token} = await Expo.Facebook.logInWithReadPermissionsAsync(ADD_ID, options)
     if (type === 'success') {
          const response = await fetch(`https://graph.facebook.com/me?access_token=${token}`)
+          console.log(token);
+          try {
+            await AsyncStorage.setItem('id_token', token);
+          } catch (error) {
+            // Error saving data
+          }
          console.log(await response.json());
-        this.authenticate(token);
+        // this.authenticate(token);
     }
-}
+  }
 
+  apiRequest = () => {
+    fetch('https://favor-me.herokuapp.com/api/favors/users/')
+      .then(function(response){
+        return response.json();
+      })
+      .then(function(json){
+        console.log(json);
+        return json;
+      })
+      .catch(function(error) {
+        console.log('There has been a problem with your fetch operation: ' + error.message);
+      });
+    };
 
 
 
@@ -36,6 +55,7 @@ export default class App extends React.Component {
           icon={{name: 'cached'}}
           title='Login With Facebook' />
         <Text>Welcome to Favor.Me!</Text>
+        <Button raised onPress={this.apiRequest} title="Test API Fetch"/>
       </View>
     );
   }
