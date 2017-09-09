591853d6a64ebc4bb8be442aae90e5b11cf98c77
Anu <srianu98@yahoo.com>
2017-09-09 01:51:06 -0400

    firebase

--- fire.js
+++ fire.js
@@ -1,11 +1,12 @@
 import firebase from 'firebase'
-var config = { /* COPY THE ACTUAL CONFIG FROM FIREBASE CONSOLE */
-  apiKey: "unreadablestuff",
-  authDomain: "your-domain-name.firebaseapp.com",
-  databaseURL: "https://your-domain-name.firebaseio.com",
-  storageBucket: "your-domain-name.appspot.com",
-  messagingSenderId: "123123123123"
-};
+var config = {
+    apiKey: "AIzaSyCZmU_EDUWoWGcGNEh1gjLobmcU0xLjhhU",
+    authDomain: "favorme-78080.firebaseapp.com",
+    databaseURL: "https://favorme-78080.firebaseio.com",
+    projectId: "favorme-78080",
+    storageBucket: "favorme-78080.appspot.com",
+    messagingSenderId: "184552140561"
+  };
 var fire = firebase.initializeApp(config);
 export default fire;
 
