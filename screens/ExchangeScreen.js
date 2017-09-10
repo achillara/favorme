e04a2a0e88df69f9703d6d1ddc00221054a9062b
Namita Dongre <ndongre898@gmail.com>
2017-09-09 22:35:35 -0400

    exchange

--- screens/ExchangeScreen.js
+++ screens/ExchangeScreen.js
@@ -1,52 +1,98 @@
-import React from 'react';
-import { ScrollView, StyleSheet, Image, TouchableOpacity, View } from 'react-native';
+import React, { Component } from 'react';
+import { ScrollView, TextInput, Picker, StyleSheet, Text, Image, TouchableOpacity, TouchableWithoutFeedback, View } from 'react-native';
 import { ExpoLinksView } from '@expo/samples';
 
-export default class ExchangeScreen extends React.Component {
-  static navigationOptions = {
-    title: 'Exchange',
-  };
+const Item = Picker.Item;
 
-  render() {
-    return (
-      <ScrollView style={styles.container}>
-		<View style={{alignItems: 'center', flexDirection: 'row'}}>
-			<View style={styles.box}>
-				<TouchableOpacity>
-					<Image
-						source={{uri: 'https://facebook.github.io/react/img/logo_og.png'}}	
-						style={{width:400, height:400}}	
+export default class ExchangeScreen extends Component {
+	
+	static navigationOptions = {
+		title: 'Exchange',
+	};
+
+	state = {
+		selected1: 'key1',
+		selected2: 'key1',
+		selected3: 'key1',
+		color: 'red',
+		mode: Picker.MODE_DIALOG,
+		text: "What's da favor?",
+	};	
+
+	changeMode () {
+		const newMode = this.state.mode === Picker.MODE_DIALOG
+		? Picker.MODE_DROPDOWN
+		: Picker.MODE_DIALOG;
+		this.setState({mode: newMode});
+	}
+
+	onValueChange (key: string, value: string) {
+		const newState = {};
+		newState[key] = value;
+		this.setState(newState);
+	}
+
+	render() {
+		return (
+			<ScrollView style={styles.container}>
+				<View style={{flex: 2, alignItems: 'center', flexDirection: 'row'}}>
+					<View style={styles.box}>
+						<TouchableOpacity>
+							<Image
+							source={require('../images/request-button.png')}	
+							style={{flex:1, height: 100, width: 120, resizeMode: 'contain'}}
+							/>
+						</TouchableOpacity>	
+					</View>
+					<View style={styles.box}>
+						<TouchableOpacity>
+							<Image
+							source={require('../images/offer-button.png')}
+							style={{flex:1, height: 100, width: 120, resizeMode: 'contain'}}
+							/>
+						</TouchableOpacity>	
+					</View>
+				</View>
+				<View style={{flex: 2, alignItems:'center'}}>
+					<Text>THE FRIEND?</Text>
+					<Picker
+						style={styles.picker}
+						selectedValue={this.state.selected2}
+						onValueChange={this.onValueChange.bind(this, 'selected2')}
+						mode="dropdown"
+					>
+						<Item label="hello" value="key0"/>
+						<Item label="world" value="key1"/>
+					</Picker>
+				</View>
+				<View style={{flex: 2, alignItems:'center'}}>
+					<TextInput
+						style={{width: 200, height: 40, backgroundColor: '#fff', borderColor: 'gray', borderWidth: 1}}
+						onChangeText={(text) => this.setState({text})}
+						value={this.state.text}
 					/>
-				</TouchableOpacity>	
-			</View>
-			<View style={styles.box}>
-				<TouchableOpacity>
-					<Image
-						source={{uri: 'https://facebook.github.io/react/img/logo_og.png'}}
-						style={{width:400, height:400}}	
-					/>
-				</TouchableOpacity>	
-			</View>
-		</View>
-      </ScrollView>
-    );
-  }
+				</View>
+			</ScrollView>
+			);
+	}
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    paddingTop: 15,
-    backgroundColor: 'lightgreen',
-  },
-  box: {
-	flex: 1,
-	paddingTop: 20,
-	marginTop: 15,
-	marginBottom: 15,
-	height: 70,
-	alignItems: 'center',
-  },
+	container: {
+		flex: 1,
+		paddingTop: 15,
+		backgroundColor: 'lightgreen',
+	},
+	box: {
+		flex: 1,
+		paddingTop: 20,
+		marginTop: 15,
+		marginBottom: 15,
+		alignItems: 'center',
+	},
+	picker: {
+		width: 250,
+	}
 });
 
 
